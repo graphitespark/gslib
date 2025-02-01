c88d1e0de2822c6272b24a5606b61a9b3af82264
Ichim8 <155743208+Ichim9@users.noreply.github.com>
2025-02-01 18:59:44 +0000

    fixed

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,7 +1,11 @@
 use graphitesparklib;
 fn main(){
-    let new = graphitesparklib::Item::new(format!("uwu"),format!("minecraft:stick"),1);
+    let mut new = graphitesparklib::Item::new(format!("minecraft:stick"),1);
+    new.set_str_tag(String::from("\\uwu"), String::from(":3"));
+    new.set_str_tag(String::from("\"bar"), String::from("foo"));
     let mut api = graphitesparklib::CCAPI::connect();
+    api.request_scope(format!("inventory"));
     let _ = api.set_inv(1, new);
     api.terminate();
 }
+eprintln
--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,11 @@
-use std::{collections::HashMap, io::{Error, ErrorKind}};
+use std::collections::HashMap;
 use tungstenite::{stream::MaybeTlsStream, Message, WebSocket};
 use std::net::TcpStream;
 fn safeify(mut text:String) -> String{
     text = text.replace("\\","\\\\");
-    text.replace("\"","\\\"")
+    return text.replace("\"","\\\"");
 }
 pub struct Item{
-    name:String,
     material:String,
     count:i32,
     int_tags:HashMap<String,i32>,
@@ -23,14 +22,16 @@ impl CCAPI{
             for (mut str_key,mut str_value) in item.str_tags.clone(){
                 str_key = safeify(str_key);
                 str_value = safeify(str_value);
-                tag_build = format!("{}{}",tag_build,format!("{{\"hypercube:{str_key}\":\"{str_value}\"}}")) // {"hypercube:terminal":"abc"}
+                tag_build = format!("{}{}",tag_build,format!("\"hypercube:{str_key}\":\"{str_value}\",")) // {"hypercube:terminal":"abc"}
             }
             for (mut int_key,int_value) in item.int_tags.clone(){
                 int_key = safeify(int_key);
-                tag_build = format!("{}{}",tag_build,format!("{{\"hypercube:{int_key}\":{int_value}}}")) // {"hypercube:terminal":"abc"}
+                tag_build = format!("{}{}",tag_build,format!("\"hypercube:{int_key}\":{int_value}b,")) // {"hypercube:terminal":"abc"}
             }
-            let build = format!("[{{count: {}, Slot: {}b, components: {{{{\"minecraft:custom_data\": {{PublicBukkitValues: {}}}}}, id: \"{}\"}}]",item.count,slot,tag_build,item.material);
+            let build = format!("[{{count: {}, Slot: {}b, components: {{\"minecraft:custom_data\": {{PublicBukkitValues: {{{}}}}}}}, id: \"{}\"}}]",item.count,slot,tag_build,item.material);
+            println!("{build}");
             let _ = self.socket.send(Message::text(format!("setinv {}",build)));
+            println!("{}",self.socket.read().unwrap().to_text().unwrap().to_string());
         }
     }
     pub fn connect() -> CCAPI{
@@ -41,20 +42,31 @@ impl CCAPI{
     pub fn terminate(&mut self){
         let _ = &self.socket.close(None);
     }
+    pub fn has_scope(&mut self,scope:String) -> bool{
+        return self.scopes.contains(&scope);
+    }
+    pub fn request_scope(&mut self,scope:String){
+        if !self.scopes.contains(&scope){
+            let _ = self.socket.send(Message::text(format!("scopes {}",scope)));
+            loop{
+                if self.socket.read().unwrap().to_text().unwrap().to_string() == "auth"{
+                    self.scopes.push(scope);
+                    break;
+                };
+            }
+        }
+    }
 }
 impl Item{
-    pub fn new(name:String,material:String,count:i32) -> Item{
+    pub fn new(material:String,count:i32) -> Item{
         let str_tags: HashMap<String, String> = HashMap::new();
         let int_tags: HashMap<String, i32> = HashMap::new();
-        Item {name,material,count,int_tags,str_tags}
+        Item {material,count,int_tags,str_tags}
     }
     pub fn set_material(&mut self,material:String){
         self.material = material;
     }
 
-    pub fn set_name(&mut self,name:String){
-        self.name = name;
-    }
     pub fn set_str_tag(&mut self,key:String,value:String){
         self.str_tags.insert(key,value);
     }
